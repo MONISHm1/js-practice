6f2bbff853ac8605fd159c1d55dd0c747f3d5f40
MONISHm1 <monishshah795@gmail.com>
2025-11-06 04:23:57 +0000

    conversion of datatypes

---
+++ 01_basics/typeConversion.js
@@ -0,0 +1,27 @@
+// let score = null;
+// console.log(score)
+// console.log(typeof(score))
+
+let score = undefined;
+let valueInNumber = Number(score)
+// console.log(score)
+// console.log(typeof(score))
+// console.log(valueInNumber)
+
+//   33 => 33
+//   "33abc" => NaN
+//  true => 1; false =0
+
+let isLoggedIn = 1
+let booleanConversion = Boolean(isLoggedIn)
+// console.log(booleanConversion);
+
+// 1 => true  0 => false;
+// "" => false
+// monish => true
+
+
+let num = 33
+ let stringConvert = String(num)
+ console.log(stringConvert);
+ console.log(typeof(stringConvert));
